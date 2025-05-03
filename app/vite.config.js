d2bb2bbae6d7c69b14388c05afc8752f937c41c9
vishvaa-vsk <kvishvaa6@gmail.com>
2025-05-03 17:24:04 +0530

    Update project configuration and dependencies; add PWA support and manifest

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'CrossLink',
+        short_name: 'CrossLink',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#317EFB',
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: '/vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          }
+        ]
+      }
+    })
+  ],
 });
